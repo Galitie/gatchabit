dcb996748a481e624d1386e4f4d881adf2ffd34f
Galit Weinfeld <galitdances@gmail.com>
2024-10-30 00:08:02 -0400

    Added ability to complete task

--- app/(components)/Task.jsx
+++ app/(components)/Task.jsx
@@ -22,6 +22,19 @@ const Task = ({ task }) => {
     }
   };
 
+  const completeTask = async () => {
+    console.log("Ticket completed!");
+    const res = await fetch(
+      process.env.NEXT_PUBLIC_HOST_URL + "/api/Tasks/" + task._id,
+      {
+        method: "DELETE",
+      }
+    );
+    if (res.ok) {
+      router.refresh();
+    }
+  };
+
   return (
     <div className="flex items-center justify-between bg-slate-300 rounded-sm px-4 py-2">
       <div className="flex justify-start gap-6 items-center">
@@ -32,7 +45,7 @@ const Task = ({ task }) => {
         <button className="flex items-center" onClick={deleteTask}>
           <FontAwesomeIcon icon={faX} className="text-red-600" />
         </button>
-        <button className="btn flex items-center">
+        <button className="btn flex items-center" onClick={completeTask}>
           <FontAwesomeIcon icon={faCheckCircle} className="text-green-400" />
         </button>
       </div>
