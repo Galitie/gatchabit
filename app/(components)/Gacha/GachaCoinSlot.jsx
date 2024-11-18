b601ab218275bc6614a85a35c9c05884f4f26fbf
Galit Weinfeld <galitdances@gmail.com>
2024-11-17 21:41:22 -0500

    If user tokens is less than 1 hide button to get items from Gacha

--- app/(components)/Gacha/GachaCoinSlot.jsx
+++ app/(components)/Gacha/GachaCoinSlot.jsx
@@ -1,19 +1,36 @@
 "use client";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation";
 
-const GachaCoinSlot = ({ item }) => {
+const GachaCoinSlot = ({ item, user }) => {
+  const router = useRouter();
   const [itemRecieved, setItemRecieved] = useState(false);
 
   const handleClick = () => {
-    console.log(item);
     setItemRecieved(true);
+    spendToken();
+  };
+
+  const spendToken = async () => {
+    const res = await fetch(process.env.NEXT_PUBLIC_HOST_URL + "/api/Users/", {
+      method: "PATCH",
+      body: JSON.stringify({ amount: 1 }),
+    });
+    if (res.ok) {
+      router.refresh(); // Refresh server components, including Tokens in Nav
+    }
   };
 
   return (
     <>
-      <button className="btn" onClick={handleClick}>
-        Insert Coin
-      </button>
+      {user.tokens > 0 ? (
+        <button className="btn" onClick={handleClick}>
+          Insert Coin
+        </button>
+      ) : (
+        ""
+      )}
+
       {itemRecieved
         ? item.map((itemDetails, itemIndex) => (
             <div key={itemIndex}>
--- app/(components)/Gacha/GachaCoinSlotServer.jsx
+++ app/(components)/Gacha/GachaCoinSlotServer.jsx
@@ -12,12 +12,26 @@ const getItem = async () => {
   }
 };
 
+async function getUser() {
+  try {
+    const res = await fetch(process.env.NEXT_PUBLIC_HOST_URL + "/api/Users", {
+      cache: "no-store", // Ensure fresh data
+    });
+    if (!res.ok) throw new Error("Failed to fetch user data");
+    return res.json();
+  } catch (error) {
+    console.error("Failed to fetch user", error);
+    return null;
+  }
+}
+
 const GachaCoinSlotServer = async () => {
   const { item } = await getItem();
+  const user = await getUser();
 
   return (
     <>
-      <GachaCoinSlot item={item} />
+      <GachaCoinSlot item={item} user={user} />
     </>
   );
 };
